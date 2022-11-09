71ed3e5a80aa1e1813c9747f935616c886b1a36d
Khem Raj Neupane <khemrajneupane@Khems-MacBook-Pro-2.local>
2022-11-09 11:27:19 +0200

    players names can be added

--- src/components/Blocks.js
+++ src/components/Blocks.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Blocks = () => {
     const [turn, setTurn] = useState('X');
@@ -7,20 +7,35 @@ const Blocks = () => {
     const [board, setBoard] = useState(Array(9).fill(''));
     const[totalMoves, setTotalMoves] = useState(0)
     const [msg, setMsg] = useState('');
+    const [firstPlayer, setFirstPlayer] = useState('');
+    const [secondPlayer, setSecondPlayer] = useState('');
 
+    const onFirstPlayerChange = (e)=>setFirstPlayer(e.target.value)
+    const onSecondPlayerChange = (e)=>setSecondPlayer(e.target.value)
 
- 
-const handleClick = (e) => {
+    const handleClick = (e) => {
     if(board[e.target.dataset.square] === '' && !winner){
     board[e.target.dataset.square] = turn;//board[0] = first array    
     e.target.innerText = turn;//it writes X or O inside every div at a click.
     setBoard(board);
+
+    if(turn==="X"){
+        e.target.classList.add(
+            'purple',
+          );
+    }else if(turn==="O"){
+        e.target.classList.add(
+            'yellow',
+          );
+    }
+   
+    console.log("main turn", turn)
     setTurn(turn === 'X' ? 'O' : 'X');   
     setTotalMoves(totalMoves + 1);
     }else if(winner){
         setMsg(`Game ended! ${winner} won!`)
     }
-    console.log(e.target.dataset.square)//displays indexes of the squares;
+    //console.log(e.target.dataset.square)//displays indexes of the squares;
 const win = findWinner();
     if(win === 'X'){
         setGameEnded(!gameEnded);
@@ -48,14 +63,29 @@ const findWinner = () =>{
         return 'draw';
         }
 }
-
     return(
-        <div className= "container">
-             <div>TicTacToe</div>
-            
-            {winner && winner !== 'draw' ? (<div>The winner is {winner}{msg?(<div>{msg}</div>):null}</div>): null}
-            {winner && winner === 'draw' ? (<div>the game is a draw</div>): null}
+        <div className= "container jumbotron">
+             <h1>Let's play TicTacToe game!!!</h1>
+          <form>
+                <div class="form-row">
+                    <label for="colFormLabelLg" class="col-sm-2 col-form-label col-form-label-lg">Email</label>
+                    <div class="col-sm-10">
+                        <input onChange={(e)=>onFirstPlayerChange(e)} type= "text" class="form-control form-control-lg" id="colFormLabelLg" placeholder="Enter your name" />
+                    </div>
+                </div>
+                <div class="form-row">
+                    <label for="colFormLabelLg" class="col-sm-2 col-form-label col-form-label-lg">Email</label>
+                    <div class="col-sm-10">
+                        <input onChange={(e)=>onSecondPlayerChange(e)} type= "text" class="form-control form-control-lg" id="colFormLabelLg" placeholder="Enter your name" />
+                    </div>
+                </div>
+            </form>
+            <h2>First Player: </h2><h3 style={{color:"purple"}}>{`${firstPlayer} ${winner && winner !== 'draw' && winner !== 'O' && "is the winner !!"}`}</h3>
+            <h2>Second Player: </h2><h3 style={{color:"purple"}}>{`${secondPlayer} ${winner && winner !== 'draw' && winner !== 'X' && "is the winner !!"}`}</h3>
            
+            <h2 style={{color:"red"}}>{winner && winner === 'draw' ? (<div>the game is a draw</div>): null}</h2>
+            
+            <form>
             <div id = "blocks" onClick ={(e)=>handleClick(e)}>
                 <div className= "square" data-square="0"></div>
                 <div className= "square" data-square="1"></div>
@@ -67,9 +97,12 @@ const findWinner = () =>{
                 <div className= "square" data-square="7"></div>
                 <div className= "square" data-square="8"></div>
             </div>
+            <button class="btn btn-primary">Play Again</button>
+            </form>
             <div>
                 
             </div>
+            
         </div>
     )
 }
